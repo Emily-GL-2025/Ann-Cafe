9bb1098cf1a3c12f008c1306c12ca2471e915335
Emily-GL-2025 <emilyglhk@gmail.com>
2026-04-06 15:48:46 +0800

    Update eg-function.py

--- eg-function.py
+++ eg-function.py
@@ -1,1 +1,13 @@
 
+#function to calculate the mean of the ratings
+def median_caculation(rating_datas):
+  rating_data.sort() #sort the list of rates
+  count = len(rating_data)
+
+#finding the median
+if count % 2 == 1:
+  return rating_data[count // 2] #odd number of datas
+else:
+  middle1 = rating_data[(count // 2) - 1]
+  middle2 = rating_data[count // 2]
+  return (middle1 + middle2) / 2 #even number of datas
